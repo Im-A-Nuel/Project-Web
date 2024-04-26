1c3b7b0ae7296998f998bc05af4a0f06169ec344
Imanuel <imanuel.putra@ti.ukdw.ac.id>
2024-04-26 16:21:35 +0700

    NEW

---
+++ main/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$databaseName = "project";
+
+$connection = new mysqli($host, $user, $pass, $databaseName) or die("Gagal Koneksi");
+
+?>
