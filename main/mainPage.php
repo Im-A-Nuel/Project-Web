3ccc5253f419782957bb1d01d41996e314110652
Imanuel <imanuel.putra@ti.ukdw.ac.id>
2024-05-14 10:30:34 +0700

    Update mainPage.php

--- main/mainPage.php
+++ main/mainPage.php
@@ -69,7 +69,7 @@ session_start();
 
 <?php
 
-$sql = "SELECT g.nama as genre, k.idKonser ,k.nama, k.tanggal, k.tempat, k.gambar, CONCAT('Rp. ', FORMAT(t.harga, 0)) as harga 
+$sql = "SELECT g.nama as genre, k.idKonser ,k.nama, DATE_FORMAT(k.tanggal, '%d %M %Y') as tanggal, k.tempat, k.gambar, CONCAT('Rp. ', FORMAT(t.harga, 0)) as harga 
 FROM konser k 
 INNER JOIN tiket t ON k.idKonser = t.idKonser 
 INNER JOIN band b ON k.idBand = b.idBand 
