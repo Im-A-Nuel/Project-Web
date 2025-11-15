70d6608fcb0e4ef25eaa75f8d9d3522f9b0441d8
Imanuel <imanuel.putra@ti.ukdw.ac.id>
2025-11-15 09:55:52 +0700

    Add concert ticketing HTML pages and styles

--- main/mainPage.php
+++ main/mainPage.php
@@ -47,20 +47,20 @@ session_start();
                             <td id="bt1"><input type="text" name="search" placeholder="Cari konser seru disini"></td>
                             <td id="bt2"><button type="submit">search</button></td>
                         </form>
-                        <?php 
-                        if(!(isset($_SESSION['username'])) && !(isset($_SESSION['firstname']))){
+                        <?php
+                        if (!(isset($_SESSION['username'])) && !(isset($_SESSION['firstname']))) {
                             echo '<td id="bt3"><a href="register.php"><button>Register</button></a></td>';
                             echo '<td id="bt4"><a href="login.php"><button>Login</button></a></td>';
-                        }else{
+                        } else {
                             echo '<td id="bt5"><a href="logout.php"><button>Logout</button></a></td>';
-                            echo '<td id="name">'.$_SESSION['firstname'].'</td>';
+                            echo '<td id="name">' . $_SESSION['firstname'] . '</td>';
                         }
                         ?>
                     </tr>
                 </tbody>
             </table>
         </div>
-        
+
 
     </header>
 
@@ -68,75 +68,75 @@ session_start();
 
         <h2>Daftar Tiket-Konser</h2>
 
-<?php
+        <?php
 
-$sql = "SELECT g.nama as genre, k.idKonser ,k.nama, DATE_FORMAT(k.tanggal, '%d %M %Y') as tanggal, k.tempat, k.gambar, k.hargaReguler as harga 
+        $sql = "SELECT g.nama as genre, k.idKonser ,k.nama, DATE_FORMAT(k.tanggal, '%d %M %Y') as tanggal, k.tempat, k.gambar, k.hargaReguler as harga 
 FROM konser k 
 INNER JOIN band b ON k.idBand = b.idBand 
 INNER JOIN genre g ON b.idGenre = g.idGenre";
 
-$result = $connection->query($sql);
+        $result = $connection->query($sql);
 
-if ($result->num_rows > 0) {
-    $current_genre = "";
-    echo "<table>";
-    $concert_counter = 0; 
-    while($row = $result->fetch_assoc()) {
-        
-        if ($current_genre != $row['genre']) {
-            if ($current_genre != "") {
-            
-                while ($concert_counter % 4 != 0) {
-                    echo "<td></td>";
-                    $concert_counter++;
-                }
-                echo "</tr>"; 
-            }
-           
-            echo "<tr>";
-            echo "<td> <h3 id='gen'>" . $row['genre'] . "</h3></td>";
-            echo "</tr>";
-            echo "<tr>";
-            echo "<td colspan='4'><hr id='line1'></td>";
-            echo "</tr>";
-            echo "<tr>";
-            $current_genre = $row['genre'];
+        if ($result->num_rows > 0) {
+            $current_genre = "";
+            echo "<table>";
             $concert_counter = 0;
+            while ($row = $result->fetch_assoc()) {
+
+                if ($current_genre != $row['genre']) {
+                    if ($current_genre != "") {
+
+                        while ($concert_counter % 4 != 0) {
+                            echo "<td></td>";
+                            $concert_counter++;
+                        }
+                        echo "</tr>";
+                    }
+
+                    echo "<tr>";
+                    echo "<td> <h3 id='gen'>" . $row['genre'] . "</h3></td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td colspan='4'><hr id='line1'></td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    $current_genre = $row['genre'];
+                    $concert_counter = 0;
+                }
+
+                echo "<td>";
+                echo "<a href='detailPage.php?idKonser=" . $row['idKonser'] . "'>";
+                echo "<div class='container'>";
+                echo "<img id='gambar' src='" . $row['gambar'] . "' alt=''>";
+                echo "<h3>" . $row['nama'] . "</h3>";
+                echo "<p>" . $row['tanggal'] . "</p>";
+                echo "<p>" . $row['tempat'] . "</p>";
+                echo "<hr id='line2'>";
+                echo "<h4>Rp. " . number_format($row['harga'], 0, ',', '.') . "</h4>";
+                echo "</div>";
+                echo "</a>";
+                echo "</td>";
+                $concert_counter++;
+
+                if ($concert_counter % 4 == 0) {
+                    echo "</tr><tr>";
+                }
+            }
+
+            while ($concert_counter % 4 != 0) {
+                echo "<td></td>";
+                $concert_counter++;
+            }
+            echo "</tr>";
+            echo "</table>";
+        } else {
+            echo "0 hasil";
         }
-    
-        echo "<td>";
-        echo "<a href='detailPage.php?idKonser=" . $row['idKonser'] . "'>";
-        echo "<div class='container'>";
-        echo "<img id='gambar' src='" . $row['gambar'] . "' alt=''>";
-        echo "<h3>" . $row['nama'] . "</h3>";
-        echo "<p>" . $row['tanggal'] . "</p>";
-        echo "<p>" . $row['tempat'] . "</p>";
-        echo "<hr id='line2'>";
-        echo "<h4>Rp. " . number_format($row['harga'], 0, ',', '.') . "</h4>";
-        echo "</div>";
-        echo "</a>";
-        echo "</td>";
-        $concert_counter++; 
 
-        if ($concert_counter % 4 == 0) {
-            echo "</tr><tr>";
-        }
-    }
-    
-    while ($concert_counter % 4 != 0) {
-        echo "<td></td>";
-        $concert_counter++;
-    }
-    echo "</tr>";
-    echo "</table>";
-} else {
-    echo "0 hasil";
-}
+        $connection->close();
 
-$connection->close();
+        ?>
 
-?>
-        
 
     </main>
 
